0929335c402eb2ecc38a0f2fb1a184a6ec3bd1c7
antoniojperez <antoniojperez@hotmail.com>
2024-06-01 19:04:04 -0400

    adding comments to some lines

--- seqClass.py
+++ seqClass.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import sys, re
+import sys, re  # This line will import the necessary library for the system
 from argparse import ArgumentParser
 
 parser = ArgumentParser(description = 'Classify a sequence as DNA or RNA')
@@ -11,10 +11,10 @@ if len(sys.argv) == 1:
     parser.print_help()
     sys.exit(1)
 
-args = parser.parse_args()
+args = parser.parse_args()     # this line will extract information from the user's input
 
-args.seq = args.seq.upper()
-if re.search('^[ACGTU]+$', args.seq):
+args.seq = args.seq.upper()  # this line will change the arguments to upper case
+if re.search('^[ACGTU]+$', args.seq):   #this line checks in all the characters belong to the list ACGTU
     if re.search('T', args.seq):
         print ('The sequence is DNA')
     elif re.search('U', args.seq):
