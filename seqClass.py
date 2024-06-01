057231c16ebdd5af29580355e744ce9556d31f1f
antoniojperez <antoniojperez@hotmail.com>
2024-06-01 19:37:15 -0400

    We fixed the .py program to be able to detect as wrong sequences those with U and T

--- seqClass.py
+++ seqClass.py
@@ -16,17 +16,24 @@ args = parser.parse_args()     # this line will extract information from the use
 args.seq = args.seq.upper()  # this line will change the arguments to upper case
 if re.search('^[ACGTU]+$', args.seq):   #this line checks in all the characters belong to the list ACGTU
     if re.search('T', args.seq):
-        print ('The sequence is DNA')
+        if re.search('U', args.seq):
+            print ('wrong sequence')
+        else:
+            print ('The sequence is DNA')
     elif re.search('U', args.seq):
-        print ('The sequence is RNA')
+        if re.search('T', args.seq):
+            print ('wrong sequence')
+        else:
+            print ('The sequence is RNA')
     else:
         print ('The sequence can be DNA or RNA')
 else:
     print ('The sequence is not DNA nor RNA')
+
 if args.motif:
     args.motif = args.motif.upper()
     print(f'Motif search enabled: looking for motif "{args.motif}" in sequence "{args.seq}"... ', end = '')
     if re.search(args.motif, args.seq):
-        print("found here")
+        print("motif FOUND")
     else:
-        print("NO ENCONTRADO")
+        print("motif NOT found")
